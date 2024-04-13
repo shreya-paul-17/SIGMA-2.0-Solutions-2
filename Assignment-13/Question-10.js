e321df8a5892c170e8f767a7f6add11918ae0dc4
shreya-paul-17 <cse.20211047@gmail.com>
2024-04-13 21:55:10 +0530

    Add new files

--- Assignment-13/Question-10.js
+++ Assignment-13/Question-10.js
@@ -1,2 +1,25 @@
 // A function that examines a sentence and counts how many times each unique word appears in it using Map
 
+function wordCounter (sentence){
+    let words = sentence.toLowerCase().split(/\s+/) // Converting the words in to lowercase and splitting them by whitespace characters
+
+    let wordFrequencyMap = new Map () //Creating a new map
+
+    // Iterating through each word in the array 
+    for (const word of words) {
+        if (word === ' ')
+        continue;
+        if (wordFrequencyMap.has(word)) {
+            wordFrequencyMap.set(word, wordFrequencyMap.get(word)+1) // If the word is present more than once in the map then the frequency will be incremented by 1
+        } else {
+            wordFrequencyMap.set(word,1) // Otherwise the frequency will remain 1
+        }
+    }
+    
+    return wordFrequencyMap
+}
+
+const sentence = "please please submit your assignment on time, your assignments are important";
+const result = wordCounter(sentence)
+
+console.log(result);
--- Assignment-13/Question-8.js
+++ Assignment-13/Question-8.js
@@ -1,2 +1,13 @@
 // A function that calculates the number of days between the current date and the event's start date
 
+function calculateRemainingDays (eventDate){
+    let current_Date = new Date ()
+    let event_date = new Date (eventDate)
+    let oneDay = 24*60*60*1000; // To calculate the number of milliseconds in one day
+    let difference = Math.abs(event_date-current_Date) // To calculate the difference between the 2 dates in milliseconds
+    let remainingDays = Math.round (difference/oneDay) // To convert it back in days
+    return remainingDays
+}
+
+const eventDate = '2024-08-31'
+console.log(calculateRemainingDays(eventDate));
